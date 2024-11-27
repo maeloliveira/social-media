3884154d4a7f7cd4d0af7370a714fbd2d1485866
mael_ <mael.jozi@gmail.com>
2024-11-26 23:17:24 -0300

    Update imports

--- src/main/java/com/media/rest/dto/CreateUserRequest.java
+++ src/main/java/com/media/rest/dto/CreateUserRequest.java
@@ -3,7 +3,12 @@ package com.media.rest.dto;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
 
+
+@Getter
+@Setter
 @Data
 public class CreateUserRequest {
     @NotBlank(message = "Name is Required")
